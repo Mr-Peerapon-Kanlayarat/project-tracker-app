9fda99a16ed090ec6e750d2124f6f9c7cfdf4645
Peerapon Kanlayarat <peerapon.klyr@gmail.com>
2025-08-14 20:19:49 +0700

    create route

--- client-app/src/app/app.component.ts
+++ client-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MainLayoutComponent,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client-app/src/app/app.routes.ts
+++ client-app/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+
+  {
+    path: 'projects',
+    component: MainLayoutComponent,
+    // canActivate: [AuthGuard],
+    children: [
+      { path: '', component: ProjectListComponent },
+      { path: 'add-project', component: AddProjectComponent },
+    ],
+  },
+  
+  { path: '**', redirectTo: '/login' },
+];
